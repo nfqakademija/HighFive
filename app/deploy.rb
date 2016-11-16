3eaa382f4fbd742dbfd05a5dc42ee7cc460e8dc2
Dainius <dainius@lva.lt>
2016-11-16 10:56:22 +0200

    Update deploy.rb
    clear cache and update database schema after deployment

--- app/deploy.rb
+++ app/deploy.rb
@@ -2,7 +2,7 @@ set :application, 'HighFive'
 set :repo_url, '#'
 
 set :deploy_to, '/home/HighFive/'
-
+set :symfony_env, 'prod'
 set :archive_cache, true
 set :branch, 'master'
 set :scm, :rsync
@@ -10,3 +10,12 @@ set :exclude, ['.git', 'node_modules', 'web/app_dev.php']
 
 set :linked_files, fetch(:linked_files, []).push('app/config/parameters.yml')
 set :linked_dirs, fetch(:linked_dirs, []).push('app/logs')
+
+namespace :deploy do
+    after :updated, :build do
+        on roles(:web) do
+            execute "cd '#{release_path}';  bin/console --env=#{fetch(:symfony_env)} cache:clear"
+            execute "cd '#{release_path}';  bin/console --env=#{fetch(:symfony_env)} doctrine:schema:update --force"
+        end
+    end
+end
