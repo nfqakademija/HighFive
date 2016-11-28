0587418d935c54bbd8d4a821a9c7bba5f97ad94b
saulensaz <saulensaz@gmail.com>
2016-11-28 20:10:09 +0200

    added loading of bone threeD model by bone page id

--- src/AppBundle/Resources/scripts/main.js
+++ src/AppBundle/Resources/scripts/main.js
@@ -4,12 +4,11 @@ $(document).ready(function () {
     }
 
     if($('#ThreeJs').length) {
-        var threeD = new skeletOnThreeD({
-            path: bonesModelsBaseDir,
-            model: boneModelName
-        });
-
-        threeD.init();
+        if(typeof boneModelName !== 'undefined') {
+            loadThreeD(bonesModelsBaseDir, boneModelName);
+        } else if(boneId != undefined && boneJsonDir != undefined) {
+            $.getJSON(boneJsonDir, loadThreeDFromJson);
+        }
     }
 });
 
@@ -30,4 +29,24 @@ function loadGame(data) {
 
         game.init();
     }
+}
+
+function loadThreeDFromJson(data) {
+    if(data.bones != undefined) {
+        var bone = data.bones[boneId - 1];
+        if(bone != null) {
+            var name = bone.image.split('.png');
+
+            loadThreeD(bonesModelsBaseDir, name[0]);
+        }
+    }
+}
+
+function loadThreeD(dir, name) {
+    var threeD = new skeletOnThreeD({
+        path: dir,
+        model: name
+    });
+
+    threeD.init();
 }
